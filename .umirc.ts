d78617679e541e0f7b8c9b2aa62cfd5819088450
lzn01 <408135242@qq.com>
2024-04-29 09:43:08 +0800

    :sparkles: 修改umi打包配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  fastRefresh: {},
+  hash: true,
   nodeModulesTransform: {
     type: 'none',
   },
+  publicPath: './',
   routes: [{ path: '/', component: '@/pages/index' }],
-  fastRefresh: {},
 });
