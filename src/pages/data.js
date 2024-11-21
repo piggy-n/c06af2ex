aa654e721f1adcef65e2534ac87be14e59c88eb2
lzn01 <408135242@qq.com>
2024-11-22 07:08:43 +0800

    :rocket: 2024134

--- src/pages/data.js
+++ src/pages/data.js
@@ -10,8 +10,12 @@ export const initMissingValues2022147 = [
 ];
 export const data2022147 = [
   {
-    id: '2024134',
+    id: '2024135',
     value: [],
+  },
+  {
+    id: '2024134',
+    value: [2, 4, 13, 16, 18, 20],
   },
   {
     id: '2024133',
@@ -1169,8 +1173,12 @@ export const data2022147 = [
 
 export const data2014001 = [
   {
-    id: '2024134',
+    id: '2024135',
     value: [],
+  },
+  {
+    id: '2024134',
+    value: [2, 4, 13, 16, 18, 20],
   },
   {
     id: '2024133',
